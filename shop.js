6397ebfd3155ab368ebf4ed3c3f249e9644a44d5
Muhammed <118899919+Asgriz@users.noreply.github.com>
2024-03-14 15:56:05 +0100

    Add files via upload

--- shop.js
+++ shop.js
@@ -1,11 +1,11 @@
-function buy(price) {
+function Addtocart(price) {
     // Get reference to the message container
     var messageContainer = document.getElementById("messageContainer");
  
     // Create a new message element
     var messageElement = document.createElement("p");
     // Set the message content
-    messageElement.textContent = "You bought a product for $" + price;
+    messageElement.textContent = "You added a product to cart (Price) $" + price;
  
     // Append the message element to the container
     messageContainer.appendChild(messageElement);

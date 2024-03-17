cce1ed8c0ebd585ca140cb41741b9ec9ef31c8ea
Muhammed <118899919+Asgriz@users.noreply.github.com>
2024-03-17 18:17:42 +0100

    Add files via upload
    fikk ikke produktene side til side
    prøvde firebase funket ikke

--- shop.js
+++ shop.js
@@ -1,11 +1,36 @@
-function Addtocart(price) {
+function Addtocart(price, messageContainer) {
     // Get reference to the message container
     var messageContainer = document.getElementById("messageContainer");
  
     // Create a new message element
     var messageElement = document.createElement("p");
     // Set the message content
-    messageElement.textContent = "You added a product to cart (Price) $" + price;
+    messageElement.textContent = "Added product to cart (Price) $" + price;
+ 
+    // Append the message element to the container
+    messageContainer.appendChild(messageElement);
+    
+}
+
+
+var addToCartCalled = false; // Flag variable to track whether function has been called
+
+function Addtocart(price, messageContainer) {
+    // Check if addToCart function has already been called
+    if (addToCartCalled) {
+        return; // Exit the function if it has already been called
+    }
+
+    // Set the flag to true to indicate that function has been called
+    addToCartCalled = true;
+
+    // Get reference to the message container
+    var messageContainer = document.getElementById("messageContainer");
+ 
+    // Create a new message element
+    var messageElement = document.createElement("p");
+    // Set the message content
+    messageElement.textContent = "Added product to cart (Price) $" + price;
  
     // Append the message element to the container
     messageContainer.appendChild(messageElement);
